25ba1aca3006a1d26bece1c18cbdddc5d1c85d56
Tarcisio Mauro <tarcisio.mauro@cloudpay.net>
2024-12-04 17:05:56 +0100

    salvando o meu arquivo main novamente

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 29
+nome = "Tarcisio"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
